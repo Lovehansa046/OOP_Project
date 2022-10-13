961b2b13eba14ae07d65e4c6b0690a33eb6f8038
Artjom plotnikov <artjomplotnikov2017@gmail.com>
2022-10-13 09:27:41 +0300

    Добавление класса App

--- src/App.java
+++ src/App.java
@@ -1,2 +1,37 @@
+import MyClasses.Author;
+import MyClasses.Book;
+
 public class App {
+
+    public void run() {
+        Book book = createBook("Евгений Онегин");
+        book.AddAuthor(createAuthor("Александр", "Пушкик"));
+
+        System.out.println(book);
+
+        Book book1 = createBook("Роман");
+        book1.AddAuthor(createAuthor("Роман", "Ломоноссов"));
+
+        System.out.println(book1);
+
+        Book book2 = createBook("Золотой цыпленок");
+        book2.AddAuthor(createAuthor("Евгений", "Петров"));
+        book2.AddAuthor(createAuthor("Илья", "Илф"));
+
+        System.out.println(book2);
+    }
+
+    private static Book createBook(String caption) {
+        Book book = new Book();
+        book.setCaption(caption);
+        return book;
+    }
+
+    private static Author createAuthor(String firstname, String lastname) {
+        Author author = new Author();
+        author.setFirstname(firstname);
+        author.setLastname(lastname);
+        return author;
+    }
 }
+
--- src/Oppproject.java
+++ src/Oppproject.java
@@ -3,33 +3,7 @@ import MyClasses.Book;
 
 public class Oppproject {
     public static void main(String[] args) {
-
-        Book book = createBook("Евгений Онегин");
-        book.AddAuthor(createAuthor("Александр", "Пушкик"));
-
-        System.out.println(book);
-
-        Book book1 = createBook("Роман");
-        book1.AddAuthor(createAuthor("Роман", "Ломоноссов"));
-
-        System.out.println(book1);
-
-        Book book2 = createBook("Золотой цыпленок");
-        book2.AddAuthor(createAuthor("Евгений", "Петров"));
-        book2.AddAuthor(createAuthor("Илья", "Илф"));
-
-        System.out.println(book2);
-
-    }
-    private static Book createBook(String caption){
-        Book book = new Book();
-        book.setCaption(caption);
-        return book;
-    }
-    private static Author createAuthor(String firstname, String lastname){
-        Author author = new Author();
-        author.setFirstname(firstname);
-        author.setLastname(lastname);
-        return author;
+        App app = new App();
+        app.run();
     }
 }
